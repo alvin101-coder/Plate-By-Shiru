270c0ffc48f783895e3e4c824cdbea08954cf3b7
Alvin Jason <alvin.kamau1@student.moringaschool.com>
2026-03-25 20:09:41 +0300

    Implemented token-based admin login with axios and toast notifications. Added sidebar navigation with routes and logout functionality.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react'
+import Login from './Components/Login'
+import Sidebar from './Components/Sidebar'
+import AddMenu from './pages/AddMenu'
+import ListMenu from './pages/ListMenu'
+import AdminTable from './pages/AdminTable'
+import { Routes , Route  } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export const backendUrl = 'http://localhost:4000'
+
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token') || "")
+
+  useEffect(() => {
+    localStorage.setItem('token', token)
+  },[token])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-white min-h-screen'>
+      <ToastContainer />
+    {
+      !token ? (<Login setToken={setToken}/>) : (
+        <>
+        <div className='flex w-full'>
+          <Sidebar setToken={setToken}/>
+          <div className='w-[70%] ml-[max(5vw, 25px my-8 text-black text-base)]'>
+            <Routes>
+              <Route path='/add' element={<AddMenu token={token} />} />
+              <Route path='/list' element={<ListMenu token={token} />} />
+              <Route path='/table' element={<AdminTable token={token} />} />
+            </Routes>
+          </div>
+
+        </div>
+        </>
+      )
+    }
+  
+    </div>
   )
 }
 
